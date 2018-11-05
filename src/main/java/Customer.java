54b9ea6a050787db2681943912f18d175c2d4885
Cristian Dina <cristi.dina93@yahoo.com>
2018-11-05 21:36:10 +0200

    Step 2

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,23 +2,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.lang.Math.toIntExact;
+
 public class Customer {
 
     public List<Fruit> buy(String inputShoppingList) {
         return Arrays.stream(inputShoppingList.split("\\W")).filter(s -> !s.isEmpty() && Arrays.stream(Fruit.values()).map(Enum::toString).collect(Collectors.toSet()).contains(s)).map(Fruit::valueOf).collect(Collectors.toList());
     }
 
-    public String checkOut(List<Fruit> shoppingCart) {
-        final int total = shoppingCart.stream().mapToInt(Fruit::getPrice).sum();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(checkOutSpecialOffers(shoppingCart));
-        stringBuilder.append("=> £" + total / 100 + "." + String.format("%02d", total % 100));
-        return stringBuilder.toString();
+    public int checkOut(List<Fruit> shoppingCart) {
+        return checkOutSpecialOffers(shoppingCart);
     }
 
-    private String checkOutSpecialOffers(List<Fruit> shoppingCart) {
-        long appleCounter = shoppingCart.stream().filter(fruit -> fruit.equals(Fruit.Apple)).count();
-        long orangeCounter = shoppingCart.stream().filter(fruit -> fruit.equals(Fruit.Orange)).count();
-        return "Because of the special offers you have received " + appleCounter + " extra Apples and " + orangeCounter/2 + " extra Oranges\n";
+    private int checkOutSpecialOffers(List<Fruit> shoppingCart) {
+        int appleCount = toIntExact(shoppingCart.stream().filter(fruit -> fruit.equals(Fruit.Apple)).count());
+        int orangeCount = toIntExact(shoppingCart.stream().filter(fruit -> fruit.equals(Fruit.Orange)).count());
+        return (appleCount / 2 + appleCount % 2) * Fruit.Apple.getPrice() + (orangeCount * 2 / 3 + orangeCount % 3) * Fruit.Orange.getPrice();
     }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,6 +1,10 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
 
@@ -25,6 +29,56 @@ public class CustomerTest {
     @Test
     public void checkOutHappyPath() {
         Customer customer = new Customer();
-        assertEquals("Because of the special offers you have received 2 extra Apples and 1 extra Oranges\n=> £1.95", customer.checkOut(Arrays.asList(Fruit.Apple, Fruit.Orange, Fruit.Orange, Fruit.Orange, Fruit.Apple)));
+        assertEquals(Fruit.Apple.getPrice() + Fruit.Orange.getPrice() * 2, customer.checkOut(Arrays.asList(Fruit.Apple, Fruit.Orange, Fruit.Orange, Fruit.Orange, Fruit.Apple)));
+    }
+
+    @Test
+    public void checkOutOrangeSpecialOffer() {
+        Customer customer = new Customer();
+        int count = 3;
+        assertEquals(getOrangesSpecialOffer(count), customer.checkOut(fillList(count, Fruit.Orange)));
+    }
+
+    @Test
+    public void checkOutOrangeSpecialOfferOneExtraOrange() {
+        Customer customer = new Customer();
+        int count = 4;
+        assertEquals(getOrangesSpecialOffer(count), customer.checkOut(fillList(count, Fruit.Orange)));
+    }
+
+    @Test
+    public void checkOutOrangeSpecialOfferTwoExtraOrange() {
+        Customer customer = new Customer();
+        int count = 5;
+        assertEquals(getOrangesSpecialOffer(count), customer.checkOut(fillList(count, Fruit.Orange)));
+    }
+
+    @Test
+    public void checkOutAppleSpecialOffer() {
+        Customer customer = new Customer();
+        int count = 2;
+        assertEquals(getApplesSpecialOffer(count), customer.checkOut(fillList(count, Fruit.Apple)));
+    }
+
+    @Test
+    public void checkOutAppleSpecialOfferOneExtraApple() {
+        Customer customer = new Customer();
+        int count = 3;
+        assertEquals(getApplesSpecialOffer(count), customer.checkOut(fillList(count, Fruit.Apple)));
+    }
+
+    private int getOrangesSpecialOffer(int count) {
+        return Fruit.Orange.getPrice() * (count * 2 / 3 + count % 3);
+    }
+
+    private int getApplesSpecialOffer(int count) {
+        return Fruit.Apple.getPrice() * (count / 2 + count % 2);
+    }
+
+    private List<Fruit> fillList(int count, Fruit fruit ){
+        List<Fruit> list = new ArrayList<>();
+        for(int i = 0 ; i< count; i++)
+            list.add(fruit);
+        return  list;
     }
 }
